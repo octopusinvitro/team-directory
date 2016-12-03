3e4a937e2980f3123cbdb7291a64fccb8416716a
private <user@example.com>
2016-12-03 16:58:50 +0000

    Add a route, view and template for the www app index

---
+++ www/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import Client, SimpleTestCase
+
+
+class HomeTest(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_loads_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('<h1>Team Directory</h1>' in str(response.content))
